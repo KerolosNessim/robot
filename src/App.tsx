98e0231553066e21e4cd50dc53e55a14d5ae0536
KerolosNessim <keronessim9@gmail.com>
2025-12-30 13:55:41 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HelmetHoverReveal from './components/HelmetHoverReveal'
+
+function App() {
+
+  return (
+    <>
+    <HelmetHoverReveal />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HelmetHoverReveal.tsx
@@ -0,0 +1,45 @@
+import React, { useRef, useState } from "react";
+
+export default function HelmetHoverReveal() {
+  const wrapRef = useRef<HTMLDivElement>(null);
+  const [pos, setPos] = useState({ x: "50%", y: "50%" });
+  const [hover, setHover] = useState(false);
+
+  const handleMove = (e: React.MouseEvent) => {
+    const el = wrapRef.current;
+    if (!el) return;
+
+    const r = el.getBoundingClientRect();
+    const x = ((e.clientX - r.left) / r.width) * 100;
+    const y = ((e.clientY - r.top) / r.height) * 100;
+
+    setPos({ x: `${x}%`, y: `${y}%` });
+  };
+
+  return (
+    <div
+      ref={wrapRef}
+      className="revealWrap"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      onMouseMove={handleMove}
+    >
+      {/* brain image */}
+      <img className="brain" src="/brain2.png" alt="brain"  />
+
+      {/* human image */}
+      <img
+        className="human"
+        src="/helmet.png"
+        alt="human"
+        style={
+          {
+            ["--x"]: pos.x,
+            ["--y"]: pos.y,
+            ["--r"]: hover ? "300px" : "0px",
+          } as React.CSSProperties
+        }
+      />
+    </div>
+  );
+}
